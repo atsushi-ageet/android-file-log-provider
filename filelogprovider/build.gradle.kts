4eee72e9a6771c13451fb02fad724093013583ca
atsushi <atsushi@ageet.com>
2018-12-13 00:27:48 +0900

    Update gradle files for kotlin DSL

---
+++ filelogprovider/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        minSdkVersion(14)
+        targetSdkVersion(28)
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    libraryVariants.all {
+        generateBuildConfig.enabled = false
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+}
