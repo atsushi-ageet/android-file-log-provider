9c66ca9f3cabeeaa85108491acf264c19ec91632
atsushi <atsushi@ageet.com>
2019-01-04 18:42:54 +0900

    Fix demo app

--- demo/src/main/java/com/ageet/filelogprovider/demo/CustomLogFormatter.kt
+++ demo/src/main/java/com/ageet/filelogprovider/demo/CustomLogFormatter.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.ageet.filelogprovider.LogFormatter
 
 class CustomLogFormatter(context: Context) : LogFormatter.Default(context) {
-    override fun formatLine(priorityText: String, tag: String, line: String, pid: Int, tid: Int, dateText: String, processShortName: String): String {
+    override fun formatLine(priorityText: String, tag: String, line: String, pid: Int, tid: Int, dateText: String): String {
         return "$dateText $priorityText/$tag($pid): $line"
     }
 }
