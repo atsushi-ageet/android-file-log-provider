9c45acd9fafa9e2411f9bce1deeaadedc8cd2c23
atsushi <atsushi@ageet.com>
2019-06-05 15:22:58 +0900

    AS 3.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.2.1")
-        classpath(kotlin("gradle-plugin", version = "1.3.11"))
+        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath(kotlin("gradle-plugin", version = "1.3.31"))
     }
 }
 
