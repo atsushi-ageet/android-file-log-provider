9fe27502b32cf4dbaca4842bf62a371cb4adf231
atsushi <atsushi@ageet.com>
2021-04-06 15:47:10 +0900

    current process name for P

--- filelogprovider/src/main/java/com/ageet/filelogprovider/LogUtils.kt
+++ filelogprovider/src/main/java/com/ageet/filelogprovider/LogUtils.kt
@@ -1,9 +1,11 @@
 package com.ageet.filelogprovider
 
 import android.app.ActivityManager
+import android.app.Application
 import android.content.Context
+import android.os.Build
 import android.os.Process
 
 private val Context.activityManager: ActivityManager get() = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
 internal fun Context.getProcessName(pid: Int): String = activityManager.runningAppProcesses?.firstOrNull { it.pid == pid }?.processName ?: "Process-$pid"
-internal val Context.currentProcessName: String get() = getProcessName(Process.myPid())
+internal val Context.currentProcessName: String get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) Application.getProcessName() else getProcessName(Process.myPid())
