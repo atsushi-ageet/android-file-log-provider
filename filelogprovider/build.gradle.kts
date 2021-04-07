b0764e0c1f78f0e2fe68d8a76dff97f11c5d5988
atsushi <atsushi@ageet.com>
2021-04-07 15:57:58 +0900

    Remove android-maven plugin

--- filelogprovider/build.gradle.kts
+++ filelogprovider/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 plugins {
     id("com.android.library")
     kotlin("android")
-    id("com.github.dcendents.android-maven") version "2.1"
 }
 
 android {
